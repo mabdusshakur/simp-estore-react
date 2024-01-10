5220943900e0eb93a159a7cb8d2532664d7ca01b
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-10 21:08:31 +0600

    set & get, user - token , added

---
+++ src/authManager.js
@@ -0,0 +1,20 @@
+let token = localStorage.getItem('token');
+let user = JSON.parse(localStorage.getItem('user'));
+
+export const setToken = (newToken) => {
+  token = newToken;
+  localStorage.setItem('token', newToken);
+};
+
+export const getToken = () => {
+  return token;
+};
+
+export const setUser = (newUser) => {
+  user = newUser;
+  localStorage.setItem('user', JSON.stringify(newUser));
+};
+
+export const getUser = () => {
+  return user;
+};
