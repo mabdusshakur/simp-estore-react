a8b79ba1589e8917dd595e708a9b22ea4868cee6
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-10 18:59:31 +0600

    router defined

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Navbar from './Components/Navbar.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home</div>,
+  },
+  {
+    path: "/login",
+    element: <div>Login</div>,
+  },
+  {
+    path: "/register",
+    element: <div>Register</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Navbar />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
