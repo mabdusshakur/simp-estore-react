043812c8f77e51d63875f7490fcdaf847e47a568
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-11 19:22:28 +0600

    routes activated for navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,15 @@
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
 
 const navigation = [
-    { name: "Dashboard", href: "#", current: true },
-    { name: "Login", href: "#", current: false },
-    { name: "Register", href: "#", current: false },
-    { name: "Logout", href: "#", current: false },
-];
+    { name: 'Home', href: '/', current: false },
+    { name: 'Dashboard', href: '/dashboard', current: false },
+    { name: 'Login', href: '/login', current: false },
+    { name: 'Register', href: '/register', current: false },
+    { name: 'Logout', href: '/logout', current: false },
+]
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(" ");
@@ -37,11 +39,12 @@ export default function Navbar() {
                                     <span className="text-white">Simp-Estore</span>
                                 </div>
                                 <div className="hidden sm:ml-6 sm:block">
+
                                     <div className="flex space-x-4">
                                         {navigation.map((item) => (
-                                            <a key={item.name} href={item.href} className={classNames(item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white", "rounded-md px-3 py-2 text-sm font-medium")} aria-current={item.current ? "page" : undefined}>
+                                            <Link key={item.name} to={item.href} className={classNames(item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'rounded-md px-3 py-2 text-sm font-medium')} aria-current={item.current ? 'page' : undefined}>
                                                 {item.name}
-                                            </a>
+                                            </Link>
                                         ))}
                                     </div>
                                 </div>
@@ -93,9 +96,9 @@ export default function Navbar() {
                     <Disclosure.Panel className="sm:hidden">
                         <div className="space-y-1 px-2 pb-3 pt-2">
                             {navigation.map((item) => (
-                                <Disclosure.Button key={item.name} as="a" href={item.href} className={classNames(item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white", "block rounded-md px-3 py-2 text-base font-medium")} aria-current={item.current ? "page" : undefined}>
+                                <Link key={item.name} as="a" href={item.href} className={classNames(item.current ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white", "block rounded-md px-3 py-2 text-base font-medium")} aria-current={item.current ? "page" : undefined}>
                                     {item.name}
-                                </Disclosure.Button>
+                                </Link>
                             ))}
                         </div>
                     </Disclosure.Panel>
