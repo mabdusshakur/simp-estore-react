b9ef5c43345192a8ae80d3cd972fc7397fb532bf
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-11 21:27:49 +0600

    dummy text added

--- src/Pages/DashboardPage.jsx
+++ src/Pages/DashboardPage.jsx
@@ -7,7 +7,9 @@ import SubCategory from './SubCategoryPage';
 function Dashboard() {
     return (
         <>
-            
+             <div className="container mx-auto px-4">
+                <h1>Dashboard</h1>
+            </div>
         </>
     );
 }
