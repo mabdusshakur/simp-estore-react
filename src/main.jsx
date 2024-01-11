41b59138a136173533804023cebfb83099b97896
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-11 19:22:43 +0600

    different routes method acced

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from './Components/Navbar.jsx';
 import Login from './Pages/LoginPage.jsx';
 import Dashboard from './Pages/DashboardPage.jsx';
@@ -19,30 +19,43 @@ const PreventIfAuthenticated = () => {
   return token ? <Navigate to="/dashboard" /> : <Outlet />;
 };
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Home</div>,
-  },
-  {
-    path: "/login",
-    element: <PreventIfAuthenticated />,
-    children: [{ index: true, element: <Login /> }]
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "/dashboard",
-    element: <ProtectedRoute />,
-    children: [{ index: true, element: <Dashboard /> },],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <div>Home</div>,
+//   },
+//   {
+//     path: "/login",
+//     element: <PreventIfAuthenticated />,
+//     children: [{ index: true, element: <Login /> }]
+//   },
+//   {
+//     path: "/register",
+//     element: <Register />,
+//   },
+//   {
+//     path: "/dashboard",
+//     element: <ProtectedRoute />,
+//     children: [{ index: true, element: <Dashboard /> },],
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter>
     <Navbar />
-    <RouterProvider router={router} />
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/login" element={<PreventIfAuthenticated />} >
+          <Route index element={<Login />} />
+        </Route>
+        <Route path="/register" element={<PreventIfAuthenticated />} >
+          <Route index element={<Register />} />
+        </Route>
+        <Route path="/dashboard" element={<ProtectedRoute />} >
+          <Route index element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
