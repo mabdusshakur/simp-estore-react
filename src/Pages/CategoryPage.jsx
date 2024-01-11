e537ae88e4d881487d981aeaeddbdfeba99e9563
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-11 20:36:44 +0600

    demo category and sub-category page added

---
+++ src/Pages/CategoryPage.jsx
@@ -0,0 +1,11 @@
+function Category() {
+    return (
+        <>
+            <div class="container mx-auto px-4">
+                <h1>Category</h1>
+            </div>
+        </>
+    );
+}
+
+export default Category;
---
+++ src/Pages/SubCategoryPage.jsx
@@ -0,0 +1,11 @@
+function SubCategory() {
+    return (
+        <>
+            <div class="container mx-auto px-4">
+                <h1>SubCategory</h1>
+            </div>
+        </>
+    );
+}
+
+export default SubCategory;
