512888e089a8ab2c623eca6dc3ca4182d35ef956
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-11 20:18:46 +0600

    sidebar added

---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { FaBoxOpen } from "react-icons/fa";
+import { FaBoxesStacked } from "react-icons/fa6";
+function Sidebar() {
+    const [nav, setNav] = useState(false);
+
+    const menuItems = [
+        { icon: <FaBoxOpen size={25} className="mr-4" />, text: "Category" },
+        { icon: <FaBoxesStacked size={25} className="mr-4" />, text: "SubCategory" },
+    ];
+    return (
+        <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4 shadow-sm">
+            {/* Left side */}
+            <div className="flex items-center">
+                <div onClick={() => setNav(!nav)} className="cursor-pointer">
+                    <AiOutlineMenu size={30} />
+                </div>
+            </div>
+
+            {/* Mobile Menu */}
+            {/* Overlay */}
+            {nav ? (
+                <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+            ) : (
+                ""
+            )}
+
+            {/* Side drawer menu */}
+            <div className={nav ? "fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300" : "fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300"}>
+                <AiOutlineClose onClick={() => setNav(!nav)} size={30} className="absolute right-4 top-4 cursor-pointer" />
+                <nav>
+                    <ul className="flex flex-col p-4 text-gray-800">
+                        {menuItems.map(({ icon, text }, index) => {
+                            return (
+                                <div key={index} className=" py-4">
+                                    <li className="text-sm flex cursor-pointer  w-[80%] rounded-full mx-auto p-2 hover:text-blue-500">
+                                        {icon} {text}
+                                    </li>
+                                </div>
+                            );
+                        })}
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    );
+
+}
+
+export default Sidebar;
