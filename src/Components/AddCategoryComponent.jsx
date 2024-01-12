49485ffb1a13bbf0fe69fad07426757a0e8af307
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-12 16:56:28 +0600

    category post added

--- src/Components/AddCategoryComponent.jsx
+++ src/Components/AddCategoryComponent.jsx
@@ -1,13 +1,24 @@
 import { useState } from "react";
+import http from "../axios";
 
 function AddCategoryComponent() {
     const [categoryName, setCategoryName] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        http.post('/admin/categories', { name : categoryName }).then((res) => {
+            if(res.status === 200) {
+                setCategoryName('');
+                alert('Category Added Successfully');
+            }
+        }).catch((err) => { console.log(err); });
+    }
     return (
         <>
             <div className="container mx-auto px-4 my-5">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="mb-6">
-                        <input type="text" id="categoryName" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Category Name" required value={categoryName} onChange={(e) => setCategoryName(e.target.value)}/>
+                        <input type="text" id="categoryName" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Category Name" required value={categoryName} onChange={(e) => setCategoryName(e.target.value)} />
                     </div>
                     <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
                 </form>
