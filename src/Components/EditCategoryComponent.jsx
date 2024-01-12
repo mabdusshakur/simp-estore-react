e040ba057bdacff5fef51ffca0abad3558e7efb7
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-12 17:14:18 +0600

    fetch category by id added

--- src/Components/EditCategoryComponent.jsx
+++ src/Components/EditCategoryComponent.jsx
@@ -1,13 +1,41 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import http from '../axios';
 
 function EditCategoryComponent() {
     const { id } = useParams();
+    const [categoryName, setCategoryName] = useState('');
 
-    return ( 
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = () => {
+        http.get(`/categories/${id}`).then((res) => {
+            const response = res.data.data;
+            setCategoryName(response.name);
+        }).catch((err) => {
+            console.log(err);
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+      
+    }
+
+    return (
         <>
-            <h1>Edit Category for {id}</h1>
+            <div className="container mx-auto px-4 my-5">
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-6">
+                        <input type="text" id="categoryName" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Category Name" required value={categoryName} onChange={(e) => setCategoryName(e.target.value)} />
+                    </div>
+                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                </form>
+            </div>
         </>
-     );
+    );
 }
 
 export default EditCategoryComponent;
