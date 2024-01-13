28607e229ddac8639b4672ac9ee3b0d7c1d88338
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-13 15:38:00 +0600

    file header added

--- src/axios.js
+++ src/axios.js
@@ -4,6 +4,7 @@ const http = axios.create({
   baseURL: "http://127.0.0.1:8000/api/v1",
   headers: {
     "Content-Type": "application/json",
+    'Content-Type': 'multipart/form-data',
     Authorization: `Bearer ${localStorage.getItem("token")}`,
   },
 });
