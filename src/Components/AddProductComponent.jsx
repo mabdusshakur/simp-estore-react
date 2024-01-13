2bc9be5afc990a96f0866044f93496bfa2952ea3
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-13 15:05:58 +0600

    necessary states added

--- src/Components/AddProductComponent.jsx
+++ src/Components/AddProductComponent.jsx
@@ -2,13 +2,23 @@ import { useEffect, useState } from "react";
 import http from "../axios";
 
 function AddProductComponent() {
-
+    const [categoryId, setCategoryId] = useState();
+    const [categories, setCategories] = useState([]);
+    const [subCategoryId, setSubCategoryId] = useState();
+    const [subCategories, setSubCategories] = useState([]);
+    const [productName, setProductName] = useState('');
+    const [regularPrice, setRegularPrice] = useState('');
+    const [salePrice, setSalePrice] = useState('');
+    const [description, setDescription] = useState('');
+    const [status, setStatus] = useState('');
+    const [stock, setStock] = useState('');
+    
     useEffect(() => {
         fetchData();
     }, []);
 
     const fetchData = () => {
-
+      
     };
 
     const handleSubmit = (e) => {
@@ -16,7 +26,7 @@ function AddProductComponent() {
     }
     return (
         <>
-           
+            
         </>
     );
 }
