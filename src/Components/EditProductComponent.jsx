f7431251018b0d1db0802fd78a93902f17394a98
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-14 10:39:27 +0600

    edit product component added

---
+++ src/Components/EditProductComponent.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import http from '../axios';
+
+function EditProductComponent() {
+    const { id } = useParams();
+   
+
+    return (
+        <>
+           
+        </>
+    );
+}
+
+export default EditProductComponent;
