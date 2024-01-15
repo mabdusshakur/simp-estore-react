5412214af48a800e6fc57d5821029a22dc5e397b
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-15 12:39:55 +0600

    async fetch added and sale,regular price conditions added

--- src/Components/Display/RecentProduct.jsx
+++ src/Components/Display/RecentProduct.jsx
@@ -4,28 +4,32 @@ import ProductCard from "../ProductCard";
 
 function RecentProduct() {
     const [products, setProducts] = useState([]);
+
     useEffect(() => {
         fetchData();
     }, []);
 
-    const fetchData = () => {
-        http.get(`/products?sort=created_at&order=desc`).then((res) => {
+    const fetchData = async () => {
+        try {
+            const res = await http.get(`/products?sort=created_at&order=desc`);
             const response = res.data.data;
             console.log(response);
             setProducts(response);
-        }).catch((err) => {
+        } catch (err) {
             console.log(err);
-        });
-    }
+        }
+    };
 
     return (
         <>
             <div className="flex flex-wrap mt-5 justify-center">
-                {
-                    products.map((product) => (
-                        <ProductCard key={product.id} name={product.name} price={product.regular_price} />
-                    ))
-                }
+                {products.map((product) => (
+                    <ProductCard
+                        key={product.id}
+                        name={product.name}
+                        price={product.sale_price != 0 ? product.sale_price : product.regular_price}
+                    />
+                ))}
             </div>
         </>
     );
