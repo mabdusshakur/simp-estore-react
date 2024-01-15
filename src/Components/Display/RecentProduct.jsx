46a3b30b23cd949b47c018289925ae089e6c3f0f
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-15 12:32:55 +0600

    recent product fetched

--- src/Components/Display/RecentProduct.jsx
+++ src/Components/Display/RecentProduct.jsx
@@ -1,10 +1,25 @@
+import { useEffect } from "react";
+import http from "../../axios";
 import ProductCard from "../ProductCard";
 
 function RecentProduct() {
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = () => {
+        http.get(`/products?sort=created_at&order=desc`).then((res) => {
+            const response = res.data.data;
+            console.log(response);
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+
     return (
         <>
             <div className="flex flex-wrap mt-5 justify-center">
-                <ProductCard name="Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport" price="599"/>
+                <ProductCard name="Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport" price="599" />
             </div>
         </>
     );
