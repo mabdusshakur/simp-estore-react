632028e268e50faf793b35f7a04999bb0bea9eef
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-15 12:28:11 +0600

    name , price props received

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,4 +1,5 @@
-function ProductCard() {
+function ProductCard(props) {
+    const { name,price } = props;
     return (
         <>
             <div className="max-w-60 mx-1 my-1 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -7,10 +8,10 @@ function ProductCard() {
                 </a>
                 <div className="px-5 pb-5">
                     <a href="#">
-                        <h5 className="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
+                        <h5 className="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{name}</h5>
                     </a>
                     <div className="flex items-center justify-between">
-                        <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+                        <span className="text-3xl font-bold text-gray-900 dark:text-white">${price}</span>
                         <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
                     </div>
                 </div>
