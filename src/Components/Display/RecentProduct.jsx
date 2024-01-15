a2c0d124dd27316ac507642e0c5e77f887f51ee0
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-15 12:24:40 +0600

    class property changed

--- src/Components/Display/RecentProduct.jsx
+++ src/Components/Display/RecentProduct.jsx
@@ -3,7 +3,7 @@ import ProductCard from "../ProductCard";
 function RecentProduct() {
     return (
         <>
-            <div class="flex flex-wrap mt-5 justify-center">
+            <div className="flex flex-wrap mt-5 justify-center">
                 <ProductCard />
             </div>
         </>
