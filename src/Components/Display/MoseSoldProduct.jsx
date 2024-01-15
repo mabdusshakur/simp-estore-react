5cc689e573174e88fd679bc71d8050097fbb2e7d
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-15 12:56:36 +0600

    most-sold product display added

---
+++ src/Components/Display/MoseSoldProduct.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import http from "../../axios";
+import ProductCard from "../ProductCard";
+
+function MostSoldProduct() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const res = await http.get(`/products?sort=sold_count&order=desc`);
+            const response = res.data.data;
+            console.log(response);
+            setProducts(response);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    return (
+        <>
+            <div className="flex flex-wrap mt-5 justify-center">
+                {products.map((product) => (
+                    <ProductCard
+                        key={product.id}
+                        name={product.name}
+                        price={product.sale_price != 0 ? product.sale_price : product.regular_price}
+                    />
+                ))}
+            </div>
+        </>
+    );
+}
+
+export default MostSoldProduct;
