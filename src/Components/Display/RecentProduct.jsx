366692f735fa01f3542e92bff7849cd408a8990d
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-15 12:21:01 +0600

    recent product component added

---
+++ src/Components/Display/RecentProduct.jsx
@@ -0,0 +1,13 @@
+import ProductCard from "../ProductCard";
+
+function RecentProduct() {
+    return (
+        <>
+            <div class="flex flex-wrap mt-5 justify-center">
+                <ProductCard />
+            </div>
+        </>
+    );
+}
+
+export default RecentProduct;
