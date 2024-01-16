986fec41bcf3b518eb3e48f03430ea3ed4cd7e93
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-16 12:10:20 +0600

    checked for wishlist exists or not

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import http from "../axios";
 import { FaHeart } from "react-icons/fa";
 
@@ -5,6 +6,9 @@ function ProductCard(props) {
     const { id, name, price, images } = props;
     const trimmedName = name.substring(0, 30);
 
+    useEffect(() => {
+        checkProductAlreadyInWishlist();
+    }, []);
     const handleAddToCart = () => {
         http.post('/carts', { product_id: id, quantity: 1 }).then((res) => {
             console.log(res);
@@ -21,6 +25,16 @@ function ProductCard(props) {
             }
         }).catch((err) => { console.log(err); });
     };
+
+    const checkProductAlreadyInWishlist = () => {
+        http.post(`/wishlist-exists/${id}`).then((res) => {
+            if(res.data === true) {
+                console.log('Product already in wishlist id = ', id);
+                document.getElementById('wishlist').classList.remove('text-white');
+                document.getElementById('wishlist').classList.add('text-red-500');
+            }
+        }).catch((err) => { console.log(err); });
+    };
     return (
         <>
             <div className="max-w-60 mx-1 my-1 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-500">
@@ -34,7 +48,7 @@ function ProductCard(props) {
                     <div className="flex items-center justify-between">
                         <span className="text-xl font-bold text-gray-900 dark:text-white">${price}</span>
                         <button onClick={handleAddToCart} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-1.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</button>
-                        <FaHeart className="text-xl text-red-500 cursor-pointer" onClick={handleWishlist}/>
+                        <FaHeart className="text-xl text-white cursor-pointer" id="wishlist" onClick={handleWishlist}/>
                     </div>
 
                 </div>
