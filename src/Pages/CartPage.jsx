02f2373a39d0395b287c7f8e9358eb5734ca3873
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-17 20:21:54 +0600

    cart completed

--- src/Pages/CartPage.jsx
+++ src/Pages/CartPage.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import http from '../axios';
+import { Link } from 'react-router-dom';
 
 function Cart() {
     const [cart, setCart] = useState([]);
@@ -7,8 +8,8 @@ function Cart() {
         fetchData();
     }, []);
 
-    const fetchData = () => {
-        http.get(`/carts`).then((res) => {
+    const fetchData = async () => {
+        await http.get(`/carts`).then((res) => {
             const response = res.data.data;
             console.log(response);
             setCart(response);
@@ -17,11 +18,27 @@ function Cart() {
         });
     };
 
-    const handleDeleteAllWishlist = (event) => {
-        http.post(`/carts/destroy-all`).then((res) => {
+    const handleDeleteAllCart = async (event) => {
+        await http.post(`/carts/destroy-all`).then((res) => {
             console.log(res);
             if (res.status === 200) {
                 alert('All Cart deleted successfully');
+                fetchData();
+            }
+        }).catch((err) => { console.log(err); });
+    };
+
+    const handleIncrement = async (event, id) => {
+        await http.post(`/carts/increment/${id}`).then((res) => {
+            if (res.status === 200) {
+                fetchData();
+            }
+        }).catch((err) => { console.log(err); });
+    };
+
+    const handleDecrement = async (event, id) => {
+        await http.post(`/carts/decrement/${id}`).then((res) => {
+            if (res.status === 200) {
                 fetchData();
             }
         }).catch((err) => { console.log(err); });
@@ -38,6 +55,9 @@ function Cart() {
                                     Product Name
                                 </th>
                                 <th scope="col" className="px-6 py-3">
+                                    Quantity
+                                </th>
+                                <th scope="col" className="px-6 py-3">
                                     Action
                                 </th>
                             </tr>
@@ -45,7 +65,7 @@ function Cart() {
                         <tbody>
                             {
                                 cart.map((cart, index) => {
-                                    const handleDeleteWishlist = (event) => {
+                                    const handleDeleteCart = (event) => {
                                         console.log(cart.id);
                                         const cartId = cart.id;
                                         http.delete(`/carts/${cartId}`).then((res) => {
@@ -67,7 +87,24 @@ function Cart() {
                                                 </div>
                                             </td>
                                             <td className="px-6 py-4 text-sm">
-                                                <button className="text-red-500 hover:text-red-700 px-1" onClick={handleDeleteWishlist}>Delete</button>
+                                                <div className="relative flex items-center max-w-[8rem]">
+                                                    <button onClick={(e) => cart.quantity > 1 ? handleDecrement(e, cart.id) : ''} type="button" id="decrement-button" data-input-counter-decrement="quantity-input" className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                                                        <svg className="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
+                                                            <path stroke="currentColor" d="M1 1h16" />
+                                                        </svg>
+                                                    </button>
+
+                                                    <p id="helper-text-explanation" className="p-1 text-sm text-gray-500 dark:text-gray-400">{cart.quantity}</p>
+
+                                                    <button onClick={(e) => handleIncrement(e, cart.id)} type="button" id="increment-button" data-input-counter-increment="quantity-input" className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                                                        <svg className="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                                            <path stroke="currentColor" d="M9 1v16M1 9h16" />
+                                                        </svg>
+                                                    </button>
+                                                </div>
+                                            </td>
+                                            <td className="px-6 py-4 text-sm">
+                                                <button className="text-red-500 hover:text-red-700 px-1" onClick={handleDeleteCart}>Delete</button>
                                             </td>
                                         </tr>
                                     );
@@ -75,8 +112,9 @@ function Cart() {
                             }
                         </tbody>
                     </table>
-                    <div className="px-6 py-4 text-sm">
-                        <button className="text-red-500 hover:text-red-700 px-1" onClick={handleDeleteAllWishlist}>Delete All</button>
+                    <div className="px-6 py-4 text-sm flex justify-between">
+                        <button className="text-red-500 hover:text-red-700 px-1" onClick={handleDeleteAllCart}>Delete All</button>
+                        <Link className="text-green-500 hover:text-green-700 px-1" to={'/checkout'}>Checkout</Link>
                     </div>
                 </div>
             </div>
