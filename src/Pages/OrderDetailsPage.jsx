4eac7e013d2b63a68e630f5283b7ea842dd9d9ce
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-20 12:41:07 +0600

    useParams added

--- src/Pages/OrderDetailsPage.jsx
+++ src/Pages/OrderDetailsPage.jsx
@@ -1,4 +1,7 @@
+import { useParams } from "react-router-dom";
+
 function OrderDetails() {
+    const { id } = useParams();
     return (
         <>
             <div className="container mx-auto px-4">
