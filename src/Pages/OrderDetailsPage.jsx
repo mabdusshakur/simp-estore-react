a5365737cb103074b2216f9ae0a4cc0ef79a74ef
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-20 13:00:09 +0600

    order user details added

--- src/Pages/OrderDetailsPage.jsx
+++ src/Pages/OrderDetailsPage.jsx
@@ -5,7 +5,8 @@ import http from "../axios";
 function OrderDetails() {
     const { id } = useParams();
     const [order, setOrder] = useState({});
-    
+    const { name, email, phone_number, address_1, address_2, city, country, postal_code } = order.user;
+
     useEffect(() => {
         fetchData();
     }, [id]);
@@ -13,24 +14,24 @@ function OrderDetails() {
     const fetchData = () => {
         http.get(`/admin/orders/${id}`).then((res) => {
             const response = res.data.data;
-            console.log(response);
-
+            setOrder(response);
         }).catch((err) => {
             console.log(err);
         });
     };
+    
     return (
         <>
             <div className="container mx-auto px-4">
                 <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                    <p className="text-sm underline m-2">Orderer Name : </p>
-                    <p className="text-sm underline m-2">Orderer Email : </p>
-                    <p className="text-sm underline m-2">Orderer Phone : </p>
-                    <p className="text-sm underline m-2">Orderer Address_1 : </p>
-                    <p className="text-sm underline m-2">Orderer Address_2 : </p>
-                    <p className="text-sm underline m-2">Orderer City : </p>
-                    <p className="text-sm underline m-2">Orderer Country : </p>
-                    <p className="text-sm underline m-2">Orderer Postal-Code : </p>
+                    <p className="text-sm underline m-2">Orderer Name : {name}</p>
+                    <p className="text-sm underline m-2">Orderer Email : {email}</p>
+                    <p className="text-sm underline m-2">Orderer Phone : {phone_number}</p>
+                    <p className="text-sm underline m-2">Orderer Address_1 : {address_1}</p>
+                    <p className="text-sm underline m-2">Orderer Address_2 : {address_2}</p>
+                    <p className="text-sm underline m-2">Orderer City : {city}</p>
+                    <p className="text-sm underline m-2">Orderer Country : {country}</p>
+                    <p className="text-sm underline m-2">Orderer Postal-Code : {postal_code}</p>
                 </div>
             </div>
         </>
