96b50e017ccea68edc299856d598634d3e5aeae1
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-20 12:40:34 +0600

    fields added

--- src/Pages/OrderDetailsPage.jsx
+++ src/Pages/OrderDetailsPage.jsx
@@ -3,7 +3,14 @@ function OrderDetails() {
         <>
             <div className="container mx-auto px-4">
                 <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-
+                    <p className="text-sm underline">Orderer Name : </p>
+                    <p className="text-sm underline">Orderer Email : </p>
+                    <p className="text-sm underline">Orderer Phone : </p>
+                    <p className="text-sm underline">Orderer Address_1 : </p>
+                    <p className="text-sm underline">Orderer Address_2 : </p>
+                    <p className="text-sm underline">Orderer City : </p>
+                    <p className="text-sm underline">Orderer Country : </p>
+                    <p className="text-sm underline">Orderer Postal-Code : </p>
                 </div>
             </div>
         </>
