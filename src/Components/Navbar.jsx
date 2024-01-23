9a0757ee68ab3dceac4f73d9cd9098e854dba983
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-23 21:51:59 +0600

    search component imported

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Link, useLocation } from "react-router-dom";
 import { isAdmin, isLoggedIn } from "../authManager";
+import SearchComponent from "./SearchComponent";
 const navigation = [
     { name: 'Home', href: '/', current: false, visible: true },
     { name: 'Cart', href: '/cart', current: false, visible: true },
@@ -78,19 +79,7 @@ export default function Navbar() {
                                     </div>
                                 </div>
                             </div>
-
-                            <form>
-                                <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-                                <div class="relative">
-                                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                                        </svg>
-                                    </div>
-                                    <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Name" />
-                                </div>
-                            </form>
-
+                            <SearchComponent />
                             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                                 {/* Profile dropdown */}
                                 <Menu as="div" className="relative ml-3">
