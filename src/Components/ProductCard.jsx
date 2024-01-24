5b982883f7888298a6caae3ea8987deff164e053
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-24 11:54:10 +0600

    product card changed

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -39,22 +39,23 @@ function ProductCard(props) {
 
     return (
         <>
-            <div className="max-w-60 mx-1 my-1 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-500">
+
+
+            <div className="max-w-60 mx-1 my-1 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                 <Link to={`/product-details/${id}`}>
-                    <img className="p-1 rounded-t-lg" src={images[0].path.replace(/\\\\/, '\\')} alt="product image" />
+                    <img className="p-8 rounded-t-lg" src="/docs/images/products/apple-watch.png" alt="product image" />
                 </Link>
                 <div className="px-5 pb-5">
                     <Link to={`/product-details/${id}`}>
-                        <h5 className="mb-2 text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{trimmedName}</h5>
+                        <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{trimmedName}</h5>
                     </Link>
-                    <div className="flex items-center justify-between">
-                        <span className="text-sm p-1 font-bold text-gray-900 dark:text-white">${price}</span>
+                    <div className="flex items-center mt-2.5 mb-5">
                         <FaHeart className="text-xl text-red-500 cursor-pointer" id="wishlist" onClick={handleWishlist} />
                     </div>
-                </div>
-
-                <div class="grid grid-cols-1">
-                    <button onClick={handleAddToCart} className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm text-center p-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</button>
+                    <div className="flex items-center justify-between">
+                        <span className="text-3xl font-bold text-gray-900 dark:text-white">${price}</span>
+                        <button onClick={handleAddToCart} className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm text-center p-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</button>
+                    </div>
                 </div>
             </div>
         </>
