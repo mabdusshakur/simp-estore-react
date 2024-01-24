9afddceef1947d78f9b69d92873c998310a5a1e9
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-24 13:14:30 +0600

    logout fixed

--- src/Pages/LogoutPage.jsx
+++ src/Pages/LogoutPage.jsx
@@ -1,14 +1,12 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import http from "../axios";
 
 function Logout() {
-    const navigate = useNavigate();
     useEffect(() => {
         localStorage.removeItem('user');
         localStorage.removeItem('token');
         http.post('/logout');
-        navigate('/');
+        window.location.href = '/';
     }, []);
     return ( 
         <div>
