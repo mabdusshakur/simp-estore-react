ccf0bb00db1428c01007548f31111b313e837976
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-24 10:59:24 +0600

    details page added

--- src/Pages/ProductDetailsPage.jsx
+++ src/Pages/ProductDetailsPage.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import http from "../axios";
+
 function ProductDetails() {
-    return (  );
+    const { id } = useParams();
+    const [product, setProduct] = useState([]);
+    useEffect(() => { fetchData() }, []);
+    const fetchData = () => {
+        http.get(`/products/${id}`).then((res) => {
+            const response = res.data.data;
+            console.log(response);
+            setProduct(response);
+        }).catch((err) => { console.log(err) });
+    };
+    return (
+        <>
+            <div className="container mx-auto px-4 my-5">
+                <div className="mb-6">
+                    <div className="grid grid-cols-4 gap-4">
+                        {
+                            // product.images.map((image, index) => {
+                            //     return (
+                            //         <div key={index}>
+                            //             <img className="h-auto max-w-40" src={image.path.replace(/\\\\/, '\\')} alt="product image" />
+                            //         </div>
+                            //     );
+                            // })
+                        }
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default ProductDetails;
