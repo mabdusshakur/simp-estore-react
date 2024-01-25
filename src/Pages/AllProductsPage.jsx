efed4aa89e9e9abf1a1c441fb374811d65997804
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-25 13:40:23 +0600

    category, sub-category filtered

--- src/Pages/AllProductsPage.jsx
+++ src/Pages/AllProductsPage.jsx
@@ -3,15 +3,24 @@ import RecentProduct from "../Components/Display/RecentProduct";
 
 function AllProducts() {
     const { categoryId, subCategoryId } = useParams();
-    
-    return (
-        <>  
-            <h1>{categoryId}</h1>
-            <h1>{subCategoryId}</h1>
 
-            {/* <div className="container mx-auto mt-2 bg-gray-800 rounded-xl">
-                <RecentProduct />
-            </div> */}
+    return (
+        <>
+            {
+                !categoryId ? (
+                    <div className="container mx-auto mt-2 bg-gray-800 rounded-xl">
+                        <h1>All Products</h1>
+                    </div>
+                ) : categoryId && !subCategoryId ? (
+                    <div className="container mx-auto mt-2 bg-gray-800 rounded-xl">
+                        <h1>Category {categoryId} Products</h1>
+                    </div>
+                ) : (
+                    <div className="container mx-auto mt-2 bg-gray-800 rounded-xl">
+                        <h1>Sub Category {subCategoryId} Products</h1>
+                    </div>
+                )
+            }
         </>
     );
 }
