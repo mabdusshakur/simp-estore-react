5131e06cfb1be75c812fe424e722439e10c31a37
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-26 11:50:04 +0600

    cleanup

--- src/Pages/DashboardPage.jsx
+++ src/Pages/DashboardPage.jsx
@@ -1,9 +1,3 @@
-import { Route, Routes } from 'react-router-dom';
-import Sidebar from '../Components/SideBar';
-import { getUser } from '../authManager';
-import Category from './CategoryPage';
-import SubCategory from './SubCategoryPage';
-
 function Dashboard() {
     return (
         <>
