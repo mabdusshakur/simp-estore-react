fb4783f5c59448e6e11c86bf6758374a34365546
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-26 19:30:19 +0600

    category tree disabled from home page

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -6,8 +6,8 @@ function Home() {
     return (
         <>
             
-                <CategoryTree />
-           
+                {/* <CategoryTree />
+            */}
 
             <div className="container mx-auto mt-2 bg-gray-800 rounded-xl">
                 <h1 className="text-xl text-white px-7 py-2">Recent Products:</h1>
